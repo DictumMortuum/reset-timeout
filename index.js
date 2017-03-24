dd5a4e869ff674699475d01ce708290093207f95
DictumMortuum <theio.vrefos@gmail.com>
2017-03-24 12:14:12 +0200

    formatting

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
-var timer = null
+var timer = null;
 
-module.exports = function(callback) {
-  clearTimeout(timer)
-  timer = setTimeout(callback, 1000)
-}
+module.exports = function (callback) {
+  clearTimeout(timer);
+  timer = setTimeout(callback, 1000);
+};
